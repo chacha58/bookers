925882af90dc8f5370457d3ed60158a051b74dd1
EC2 Default User <117337820+chacha58@users.noreply.github.com>
2022-12-25 13:51:33 +0000

    kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,22 @@
 class BooksController < ApplicationController
-
-    def create
-        @book = Book.new(book_params)
-
-        if @book.save!
-        redirect_to book_path(@book.id)
-        else
-        render :index
-
-        end
-    end
-
+    
     def index
         @book = Book.new
         @books = Book.all
+    end
+    
+    def create
+        @book = Book.new(book_params)
+
+        if @book.save
+        redirect_to book_path(@book.id)
+        flash[:notice] = "Book was successfully created."
+        else
+        @books = Book.all
+        flash[:notice] = "2 errors prohibited this book from being saved:"
+        render :index
+
+        end
     end
 
     def show
@@ -29,8 +32,12 @@ class BooksController < ApplicationController
         
         if @book.update(book_params)
          redirect_to book_path(@book.id)
+         flash[:notice] = "Book was successfully created."
         else
+         @books = Book.all
+         flash[:notice] = "2 errors prohibited this book from being saved:"
          render :edit
+         
         end
     end
 
@@ -38,6 +45,7 @@ class BooksController < ApplicationController
         @book = Book.find(params[:id])  # データ（レコード）を1件取得
         @book.destroy  # データ（レコード）を削除
         redirect_to '/books'  # 投稿一覧画面へリダイレクト
+        flash[:notice] = "Book was successfully destroyed."
     end
 
     private
